27f4f4dcaa28ab329338de871c7f61e1bef83ef8
연지201 <201@192.168.56.1>
2017-11-10 17:56:56 +0900

    회원가입 서비스단 처리 화원가입 버튼 클릭 시 메인 페이지이동

---
+++ LetsMeet/src/main/java/com/TeamSecond/letsmeet/Service/MemberServiceImpl.java
@@ -0,0 +1,49 @@
+package com.TeamSecond.letsmeet.Service;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.TeamSecond.letsmeet.DTO.MemberDTO;
+import com.TeamSecond.letsmeet.IService.MemberService;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+	
+	final String AUTHMSGTRUE = "인증번호 일치";
+	final String AUTHMSGFALSE = "인증번호 불일치";
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberServiceImpl.class);
+	
+	@Override
+	public int chkIdProc(String id) {
+		if("hygi".equals(id)){
+			return 1;
+		}
+		return 0;
+	}
+
+	@Override
+	public String authSendProc() {
+		String authNumForm = String.format("%01d", (int)(Math.random()*10));
+		return authNumForm;
+	}
+
+	@Override
+	public int authChkProc(String authNum,String authNumSession) {
+		if(authNumSession.equals(authNum)){
+			return 0;
+		}
+		return 1;
+	}
+
+	@Override
+	public void memberProc(MemberDTO memberDTO) {
+		logger.info("아이디 : " + memberDTO.getId());
+		logger.info("비밀번호 : " + memberDTO.getPw());
+		logger.info("이메일 : " + memberDTO.getEmail());
+	}
+
+}
